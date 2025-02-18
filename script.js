33b5b5e3be22b0c6cc7cc8832d0966c715622fca
Shaun Bokowski <shaunbokowski@gmail.com>
2025-02-17 22:47:16 -0500

    intial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// javascript web  / 20 projects
+// basically our browser window is represented by
+// som†hing called our window and this allows us
+// to log certain data that can we can then see
+// for our javascript in our browser
+// we are colling  an api and we want to just see
+// how the data is formatted  we can consol log 
+// that data , pass it into our browser
+// in the single quotes with a string  then we will
+// thrwo in a semicolor at the end 
+  
